f4d578fbfb3b6f547978192a484bd2964b751d53
ousttrue <ousttrue@gmail.com>
2024-07-22 14:41:49 +0900

    imgui & cube

--- src/cube.glsl
+++ src/cube.glsl
@@ -22,7 +22,7 @@ in vec4 color;
 out vec4 frag_color;
 
 void main() {
-    frag_color = vec4(1,1,1,1);//color;
+    frag_color = color;
 }
 @end
 
--- src/main.zig
+++ src/main.zig
@@ -5,9 +5,18 @@ const sg = sokol.gfx;
 const sapp = sokol.app;
 const sglue = sokol.glue;
 const simgui = sokol.imgui;
+const vec3 = @import("math.zig").Vec3;
+const mat4 = @import("math.zig").Mat4;
+const shd = @import("cube.glsl.zig");
 
 const state = struct {
     var pass_action: sg.PassAction = .{};
+    // var pass_action: sg.PassAction = .{};
+    var rx: f32 = 0.0;
+    var ry: f32 = 0.0;
+    var pip: sg.Pipeline = .{};
+    var bind: sg.Bindings = .{};
+    const view: mat4 = mat4.lookat(.{ .x = 0.0, .y = 1.5, .z = 6.0 }, vec3.zero(), vec3.up());
 };
 
 export fn init() void {
@@ -21,11 +30,87 @@ export fn init() void {
         .logger = .{ .func = slog.func },
     });
 
+    // cube vertex buffer
+    state.bind.vertex_buffers[0] = sg.makeBuffer(.{
+        .data = sg.asRange(&[_]f32{
+            // positions        colors
+            -1.0, -1.0, -1.0, 1.0, 0.0, 0.0, 1.0,
+            1.0,  -1.0, -1.0, 1.0, 0.0, 0.0, 1.0,
+            1.0,  1.0,  -1.0, 1.0, 0.0, 0.0, 1.0,
+            -1.0, 1.0,  -1.0, 1.0, 0.0, 0.0, 1.0,
+
+            -1.0, -1.0, 1.0,  0.0, 1.0, 0.0, 1.0,
+            1.0,  -1.0, 1.0,  0.0, 1.0, 0.0, 1.0,
+            1.0,  1.0,  1.0,  0.0, 1.0, 0.0, 1.0,
+            -1.0, 1.0,  1.0,  0.0, 1.0, 0.0, 1.0,
+
+            -1.0, -1.0, -1.0, 0.0, 0.0, 1.0, 1.0,
+            -1.0, 1.0,  -1.0, 0.0, 0.0, 1.0, 1.0,
+            -1.0, 1.0,  1.0,  0.0, 0.0, 1.0, 1.0,
+            -1.0, -1.0, 1.0,  0.0, 0.0, 1.0, 1.0,
+
+            1.0,  -1.0, -1.0, 1.0, 0.5, 0.0, 1.0,
+            1.0,  1.0,  -1.0, 1.0, 0.5, 0.0, 1.0,
+            1.0,  1.0,  1.0,  1.0, 0.5, 0.0, 1.0,
+            1.0,  -1.0, 1.0,  1.0, 0.5, 0.0, 1.0,
+
+            -1.0, -1.0, -1.0, 0.0, 0.5, 1.0, 1.0,
+            -1.0, -1.0, 1.0,  0.0, 0.5, 1.0, 1.0,
+            1.0,  -1.0, 1.0,  0.0, 0.5, 1.0, 1.0,
+            1.0,  -1.0, -1.0, 0.0, 0.5, 1.0, 1.0,
+
+            -1.0, 1.0,  -1.0, 1.0, 0.0, 0.5, 1.0,
+            -1.0, 1.0,  1.0,  1.0, 0.0, 0.5, 1.0,
+            1.0,  1.0,  1.0,  1.0, 0.0, 0.5, 1.0,
+            1.0,  1.0,  -1.0, 1.0, 0.0, 0.5, 1.0,
+        }),
+    });
+
+    // cube index buffer
+    state.bind.index_buffer = sg.makeBuffer(.{
+        .type = .INDEXBUFFER,
+        .data = sg.asRange(&[_]u16{
+            0,  1,  2,  0,  2,  3,
+            6,  5,  4,  7,  6,  4,
+            8,  9,  10, 8,  10, 11,
+            14, 13, 12, 15, 14, 12,
+            16, 17, 18, 16, 18, 19,
+            22, 21, 20, 23, 22, 20,
+        }),
+    });
+
+    // shader and pipeline object
+    var pip_desc: sg.PipelineDesc = .{
+        .shader = sg.makeShader(shd.cubeShaderDesc(sg.queryBackend())),
+        .index_type = .UINT16,
+        .depth = .{
+            .compare = .LESS_EQUAL,
+            .write_enabled = true,
+        },
+        .cull_mode = .BACK,
+    };
+    pip_desc.layout.attrs[shd.ATTR_vs_position].format = .FLOAT3;
+    pip_desc.layout.attrs[shd.ATTR_vs_color0].format = .FLOAT4;
+    state.pip = sg.makePipeline(pip_desc);
+
     // initial clear color
     state.pass_action.colors[0] = .{
         .load_action = .CLEAR,
         .clear_value = .{ .r = 0.0, .g = 0.5, .b = 1.0, .a = 1.0 },
     };
+}
+
+fn mat4_to_array(m: *const mat4) *const [16]f32 {
+    return @ptrCast(m);
+}
+
+fn computeVsParams(rx: f32, ry: f32) shd.VsParams {
+    const rxm = mat4.rotate(rx, .{ .x = 1.0, .y = 0.0, .z = 0.0 });
+    const rym = mat4.rotate(ry, .{ .x = 0.0, .y = 1.0, .z = 0.0 });
+    const model = mat4.mul(rxm, rym);
+    const aspect = sapp.widthf() / sapp.heightf();
+    const proj = mat4.persp(60.0, aspect, 0.01, 10.0);
+    return shd.VsParams{ .mvp = mat4_to_array(&mat4.mul(mat4.mul(proj, state.view), model)).* };
 }
 
 export fn frame() void {
@@ -36,6 +121,12 @@ export fn frame() void {
         .delta_time = sapp.frameDuration(),
         .dpi_scale = sapp.dpiScale(),
     });
+
+    // cube
+    const dt: f32 = @floatCast(sapp.frameDuration() * 60);
+    state.rx += 1.0 * dt;
+    state.ry += 2.0 * dt;
+    const vs_params = computeVsParams(state.rx, state.ry);
 
     //=== UI CODE STARTS HERE
     ig.igShowDemoWindow(null);
@@ -48,7 +139,15 @@ export fn frame() void {
 
     // call simgui.render() inside a sokol-gfx pass
     sg.beginPass(.{ .action = state.pass_action, .swapchain = sglue.swapchain() });
+
+    // cube
+    sg.applyPipeline(state.pip);
+    sg.applyBindings(state.bind);
+    sg.applyUniforms(.VS, shd.SLOT_vs_params, sg.asRange(&vs_params));
+    sg.draw(0, 36, 1);
+    
     simgui.render();
+
     sg.endPass();
     sg.commit();
 }
@@ -76,4 +175,3 @@ pub fn main() void {
         .logger = .{ .func = slog.func },
     });
 }
-
