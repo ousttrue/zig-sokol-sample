7a455d056ee0eb22938bd9e8fb6840add7c7a6bf
ousttrue <ousttrue@gmail.com>
2024-07-22 14:14:30 +0900

    init

---
+++ build.zig
@@ -0,0 +1,66 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const dep_sokol = b.dependency("sokol", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "zigwrap",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("sokol", dep_sokol.module("sokol"));
+    exe.step.dependOn(buildShaders(b, target, "src/cube.glsl"));
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    b.step("run", "Run the app").dependOn(&run_cmd.step);
+}
+
+// a separate step to compile shaders, expects the shader compiler in ../sokol-tools-bin/
+// TODO: install sokol-shdc via package manager
+fn buildShaders(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    comptime shader: []const u8,
+) *std.Build.Step {
+    const sokol_tools_bin_dir = "../../floooh/sokol-tools-bin/bin/";
+    const optional_shdc: ?[:0]const u8 = comptime switch (builtin.os.tag) {
+        .windows => "win32/sokol-shdc.exe",
+        .linux => "linux/sokol-shdc",
+        .macos => if (builtin.cpu.arch.isX86()) "osx/sokol-shdc" else "osx_arm64/sokol-shdc",
+        else => null,
+    };
+    if (optional_shdc == null) {
+        std.log.warn("unsupported host platform, skipping shader compiler step", .{});
+        return;
+    }
+    const shdc_path = sokol_tools_bin_dir ++ optional_shdc.?;
+    const shdc_step = b.step("shaders", "Compile shaders (needs ../../floooh/sokol-tools-bin)");
+    const glsl = if (target.result.isDarwin()) "glsl410" else "glsl430";
+    const slang = glsl ++ ":metal_macos:hlsl5:glsl300es:wgsl";
+    const cmd = b.addSystemCommand(&.{
+        shdc_path,
+        "-i",
+        shader,
+        "-o",
+        shader ++ ".zig",
+        "-l",
+        slang,
+        "-f",
+        "sokol_zig",
+    });
+    shdc_step.dependOn(&cmd.step);
+    return shdc_step;
+}
