a2ef2cc4a10ffecfecf7ea95c29d857f7c4a7d8d
ousttrue <ousttrue@gmail.com>
2024-10-12 09:27:39 +0900

    mipmap

--- build_example.zig
+++ build_example.zig
@@ -101,7 +101,11 @@ pub const examples = [_]Example{
     // - [ ] [layerrender](sokol_examples/layerrender-sapp.zig)
     // - [ ] [primtypes](sokol_examples/primtypes-sapp.zig)
     // - [ ] [uvwrap](sokol_examples/uvwrap-sapp.zig)
-    // - [ ] [mipmap](sokol_examples/mipmap-sapp.zig)
+    .{
+        .name = "mipmap",
+        .root_source = "sokol_examples/mipmap-sapp.zig",
+        .shader = "sokol_examples/mipmap-sapp.glsl",
+    },
     // - [ ] [uniformtypes](sokol_examples/uniformtypes-sapp.zig)
     // - [ ] [blend](sokol_examples/blend-sapp.zig)
     // - [ ] [sdf](sokol_examples/sdf-sapp.zig)
